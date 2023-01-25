4a65e8fabdae1b3937237b30893b7b1e7761ec4c
Andres Felipe Alzate Alvarez <aalzatea@gmail.com>
2023-01-24 20:33:48 -0500

    Learning about typing and numbers in TypeScript

---
+++ TypeScript/Platzi_Fundamentos_de_TypeScript/src/03-typing.ts
@@ -0,0 +1,19 @@
+/*(() => {
+  let productName = 'Product 1';
+  let productPrice = 123;
+
+  productName = 'change name';
+  productName.toUpperCase();
+
+  const productStock = 1000;
+})();*/
+
+export {}
+
+let productName = 'Product 1';
+let productPrice = 123;
+
+productName = 'change name';
+productName.toUpperCase();
+
+const productStock = 1000;
